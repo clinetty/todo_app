a833bd3125ba14f2c1efc67311f43e83aa785192
Clinton <clintogechi@gmail.com>
2023-02-03 19:29:28 +0300

    Adding styling

--- flutter_fun/lib/main.dart
+++ flutter_fun/lib/main.dart
@@ -24,47 +24,132 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.black,
-          onPressed: () {
-            print('floating');
-          },
-          child: Icon(Icons.add),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        onPressed: () {
+          print('floating');
+        },
+        child: Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text('Hello Flutter'),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.settings),
+          onPressed: (() {
+            print('settings');
+          }),
         ),
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: Text('Hello Flutter'),
-          centerTitle: true,
-          leading: IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: (() {
-              print('settings');
-            }),
-          ),
-          actions: [
-            IconButton(
-                icon: Icon(Icons.search),
-                onPressed: () {
-                  print('menu');
-                }),
-            IconButton(
-                icon: Icon(Icons.email),
-                tooltip: 'mailto',
-                onPressed: () {
-                  print('menu');
-                }),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                print('menu');
+              }),
+          IconButton(
+              icon: Icon(Icons.email),
+              tooltip: 'mailto',
+              onPressed: () {
+                print('menu');
+              }),
+        ],
+      ),
+      body: Container(
+        margin: EdgeInsets.all(6.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: 500.0,
+              height: 300.0,
+              child: Image.network(
+                "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8aGFtYnVyZ2VyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60",
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 10.0),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(
+                      "Cheese Burger",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 30.0),
+                    ),
+                  ),
+                  IconButton(
+                    iconSize: 60.0,
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.favorite_border,
+                      color: Colors.red,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.call,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Text('Call')
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.directions,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Text('Directions')
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.share,
+                          color: Colors.blue,
+                        ),
+                      ),
+                      Text('Share')
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20.0),
+            ),
+            Container(
+                child: Expanded(
+              child: Text(
+                "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+                softWrap: true,
+                style: TextStyle(fontSize: 15.0),
+              ),
+            )),
           ],
         ),
-        body: Center(
-          child: Container(
-            width: 300.0,
-            height: 260.0,
-            decoration: BoxDecoration(
-                border: Border.all(
-              width: 2.0,
-            )),
-            
-          ),
-        ));
+      ),
+    );
   }
 }
