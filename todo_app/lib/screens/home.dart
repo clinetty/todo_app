31e37cd230588635744131014349d9f6b904631e
clinetty <clintogechi@gmail.com>
2023-03-14 22:32:11 +0300

    Initial commit

---
+++ todo_app/lib/screens/home.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/constants/colors.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: tdBGColor,
+        elevation: 0,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Icon(
+              Icons.menu,
+              color: tdBlack,
+              size: 30,
+            ),
+            Container(
+              height: 40,
+              width: 40,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.asset('assets/images/avatar.jpg'),
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Container(
+        child: Text('This is the home screen'),
+      ),
+    );
+  }
+}
