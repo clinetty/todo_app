c4aab92593008b82e0f48dfe59fa56944fa52778
clinetty <clintogechi@gmail.com>
2023-03-15 22:55:07 +0300

    Refactored code

--- todo_app/lib/screens/home.dart
+++ todo_app/lib/screens/home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, sized_box_for_whitespace, avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
 import 'package:todo_app/constants/colors.dart';
 
@@ -7,30 +9,43 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: tdBGColor,
-        elevation: 0,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      backgroundColor: tdBGColor,
+      appBar: _buildAppBar(),
+      body: Container(
+        child: Column(
           children: [
-            Icon(
-              Icons.menu,
-              color: tdBlack,
-              size: 30,
-            ),
             Container(
-              height: 40,
-              width: 40,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: Image.asset('assets/images/avatar.jpg'),
-              ),
+              decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(Radius)),
             ),
           ],
         ),
       ),
-      body: Container(
-        child: Text('This is the home screen'),
+    );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Icon(
+            Icons.menu,
+            color: tdBlack,
+            size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.jpg'),
+            ),
+          ),
+        ],
       ),
     );
   }
